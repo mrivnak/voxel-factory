2f18d572fbfeaa7cf362b48a092af33911aa3faa
Michael Rivnak <rivnakm1@gmail.com>
2023-09-23 21:13:23 -0400

    add empty library and gdextension example

---
+++ lib/voxelfactory/src/voxelfactory.cpp
@@ -0,0 +1,5 @@
+#include "voxelfactory.hpp"
+
+double return42() {
+    return 42.0;
+}
---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.llvmPackages_16.stdenv.mkDerivation {
+  name = "godot-nix-shell";
+  buildInputs = with pkgs; [
+    catch2_3
+    clang_16
+    cmake
+    godot_4
+    ninja
+    python311
+  ];
+  shellHook = ''
+    export CMAKE_GENERATOR=Ninja
+  '';
+}
+
